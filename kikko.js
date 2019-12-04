51a188575257009d62dd18a98332df6cfd7189ca
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-12-04 12:55:20 +0900

    1255

--- kikko.js
+++ kikko.js
@@ -1,193 +1,32 @@
 
-var buffer=document.getElementById("buffer").getContext('2d');
-var canvas=document.getElementById("buffer");
-var video=document.getElementById("video");
-var img = document.getElementById('image');
-let frame = 0;
-var reader = new FileReader();
-var flg=1;
-
-var w;
-var h;
-
-document.getElementById("alive").style.display="none";
-
-
-function draw()
+function first()
 {
-    requestAnimationFrame(draw)
-    frame++;
-    if (frame % 60 !== 0)
-    {
-    return;
-    }
-    buffer.drawImage(video, 0,0,w,h);
-    //img.src = document.getElementById("buffer").toDataURL('image/jpeg')
-
-
-
-uploadCanvasData();
-
-
-/*ローカルでファイル生成する場合は以下追加
-      var a = document.createElement('a') //download属性を持ったaタグをクリックするとダウンロードができるので、それをシミュレートする
-      document.body.appendChild(a)
-      a.style = 'display:none'
-      a.href = img.src ;
-      var day = new Date();
-      a.download = day + '.jpg'
-      a.click()
-      //createされた、objUrlを解放
-      window.URL.revokeObjectURL(img.src)
-*/
-  }
-
-
-  function stt()
-{
-
-    flg=0;
-    document.getElementById("video").style.display="none";
-    uploadCanvasData();
-    document.getElementById("alive").style.display="";
-}
-
-
-function stp()
-{
-    flg=1;
-    document.getElementById("video").style.display="";
-    video.style.width=String(w)/3 + "px";
-    video.style.height=String(h)/3 + "px";
-    document.getElementById("alive").style.display="none";
-
-}
-
-//draw
-
-
-
-
-
-const medias =
-{
-  audio: false,
-  video: {
-    facingMode: "environment",
-        width: { ideal: 1920 },
-        height: { ideal: 1080 }
-    //aspectRatio: {exact: 1.7777777778}
-    //facingMode: "user" // フロントカメラにアクセス
-  }
-};
-
-
-navigator.mediaDevices = navigator.mediaDevices ||
-((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-   getUserMedia: function(c) {
-     return new Promise(function(y, n) {
-       (navigator.mozGetUserMedia ||
-        navigator.webkitGetUserMedia).call(navigator, c, y, n);
-     });
-   }
-} : null
-);
-
-
-
-if(navigator.mediaDevices)
-{
-
-const promise = navigator.mediaDevices.getUserMedia(medias);
-
-promise.then(successCallback)
-       .catch(errorCallback);
-
-
-function successCallback(stream)
- {
-  video.srcObject = stream;
-   
-
-  var settings = stream.getVideoTracks()[0].getSettings();
-  w = settings.width;
-  h = settings.height;  
-   
-   console.log(w);
-   
-  //w=1536;
-  //h=2048;
-
-document.getElementById("buffer").setAttribute("width", w);
-document.getElementById("buffer").setAttribute("height", h);
-document.getElementById("image").setAttribute("width", w);
-document.getElementById("image").setAttribute("height", h);
-
-document.getElementById("video").style.display="none";
-document.getElementById("buffer").style.display="none";
-
-
-/*
-canvas.width *= devicePixelRatio;
-canvas.height *= devicePixelRatio;
-
-canvas.style.width = String(canvas.width / devicePixelRatio) + "px";
-canvas.style.height = String(canvas.height / devicePixelRatio) + "px";
-*/
-
-
- }
-
-function errorCallback(err)
- {
-  alert(err);
- };
-
-}else
-{
-
-}
-
-document.getElementById("video").play();
-
-draw()
-
-
-//blob size 0.95
-function uploadCanvasData()
-{
-  if(flg == 1){stp()}else
+  setTimeout(function()
   {
-    var base64 = document.getElementById("buffer").toDataURL('image/jpeg',0.95);
+  document.getElementById("fall").style.WebkitAnimationPlayState="running";
+  document.getElementById("fall").style.animationPlayState="running";
+  }
+,3000);
 
-    // Base64からバイナリへ変換
-    var bin = atob(base64.replace(/^.*,/, ''));
-    var buffer = new Uint8Array(bin.length);
-    for (var i = 0; i < bin.length; i++)
-    {
-      buffer[i] = bin.charCodeAt(i);
-    }
-    // Blobを作成
-    var blob = new Blob([buffer.buffer],
-    {
-     type: "image/jpeg"
-    });
+  setTimeout(function()
+  {
+  document.getElementById("mini").style.WebkitAnimationPlayState="running";
+  document.getElementById("mini").style.animationPlayState="running";
+  document.getElementById("fall2").style.WebkitAnimationPlayState="running";
+  document.getElementById("fall2").style.animationPlayState="running";
+  }
+,9000);
 
-    var formData = new FormData();
-    var day = new Date();
-    formData.append("image",blob, "txtime" + day + '.jpg');
-
-    console.log(blob.size);
-
-    var request = new XMLHttpRequest();
-
-    request.open("POST", "./fpsnode.js");
-    request.responseType = 'blob';
-    request.send(formData);
-
-   }
-
+    setTimeout(function()
+  {
+  document.getElementById("mini2").style.WebkitAnimationPlayState="running";
+  document.getElementById("mini2").style.animationPlayState="running";
+  document.getElementById("slide").style.WebkitAnimationPlayState="running";
+  document.getElementById("slide").style.animationPlayState="running";
+  }
+,15000);
 }
+
 
 
 
--- kikkonode.js
+++ kikkonode.js
@@ -1,5 +1,4 @@
 
-
 var multiparty = require('multiparty');
 var http = require('http');
 var util = require('util');
@@ -12,46 +11,31 @@ var url = req.url;
   console.log(url);
   console.log(req.method)
 
-  if ("/" == url)
-  {
-    fs.readFile("./index.html", "UTF-8", function (err, data)
+
+    fs.readFile("./index2.html", "UTF-8", function (err, data)
     {
       res.writeHead(200, {"Content-Type": "text/html"});
       res.write(data);
-      res.end();
+      res.end(html);
     });
-  } else if ("/fps.js" == url)
-  {
-    fs.readFile("./fps.js", "UTF-8", function (err, data)
+
+
+   if(req.method === 'POST')
     {
-      res.writeHead(200, {"Content-Type": "text/plain"});
-      res.write(data);
-      res.end();
-    });
-  }else if(req.method === 'POST')
-   {
 
       var form = new multiparty.Form();
 
       form.parse(req, function(err, fields, files)
       {
-      res.writeHead(200, {'content-type': 'image/jpeg'});
+      res.writeHead(200, {'content-type': 'text/plain'});
       res.write('received upload:\n\n');
+      res.write(fields[0]);
 
-      var day = new Date();
-      console.log(files.image[0].originalFilename);
+      console.log(fields[0]);
       //console.log(files.name);
       //  "tmp/" + day + ".jpg"
-      fs.writeFile("tmp/" + files.image[0].originalFilename, fs.readFileSync(files.image[0].path), function (err)
-        {
-            if (err)
-            {
-                console.log('ERROR:: ' + err);
-                throw err;
-            }
-        });
+
       res.end(util.inspect({fields: fields, files: files}));
       })
     };
-
    }).listen(8080);
