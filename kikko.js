f075f751531755b2cddf6b4035a3d53196c69ac4
島川徹平 <shimakawateppei@sitetsuMac.local>
2020-01-17 17:26:17 +0900

    448

--- kikko.js
+++ kikko.js
@@ -8,24 +8,6 @@ function first()
   document.getElementById("fall").style.animationPlayState="running";
   }
 ,3000);
-
-  setTimeout(function()
-  {
-  document.getElementById("mini").style.WebkitAnimationPlayState="running";
-  document.getElementById("mini").style.animationPlayState="running";
-  document.getElementById("fall2").style.WebkitAnimationPlayState="running";
-  document.getElementById("fall2").style.animationPlayState="running";
-  }
-,9000);
-
-    setTimeout(function()
-  {
-  document.getElementById("mini2").style.WebkitAnimationPlayState="running";
-  document.getElementById("mini2").style.animationPlayState="running";
-  document.getElementById("slide").style.WebkitAnimationPlayState="running";
-  document.getElementById("slide").style.animationPlayState="running";
-  }
-,15000);
 }
 
 
